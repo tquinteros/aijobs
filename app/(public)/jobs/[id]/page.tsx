89a0dfb73254418dfb50e45aba3edae5faad8b50
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2026-03-09 16:06:19 -0300

    fix 2

--- app/(public)/jobs/[id]/page.tsx
+++ app/(public)/jobs/[id]/page.tsx
@@ -1,9 +1,12 @@
 import JobDetails from '@/components/jobs/job-details';
+import { Suspense } from 'react';
 
 export default function JobIdPage() {
     return (
+        <Suspense fallback={null}>
         <div className="container mx-auto py-8">
             <JobDetails />
         </div>
+        </Suspense>
     )
 }
