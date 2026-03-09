ce80420294b9be70c3a116c4cfae8b1be4245074
Tomas Quinteros <109175498+tquinteros@users.noreply.github.com>
2026-03-09 15:02:31 -0300

    metadta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "AI Jobs",
+  description: "AI Jobs is a platform that helps you find your perfect job.",
 };
 
 const geistSans = Geist({
